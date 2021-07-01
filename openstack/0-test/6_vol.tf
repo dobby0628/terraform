4d1a3f01ac479c3384b4455065bd227e4d82db91
dobby0628 <wls7911@naver.com>
2021-07-01 04:37:10 -0400

    success lb

--- openstack/0-test/6_vol.tf
+++ openstack/0-test/6_vol.tf
@@ -1,7 +1,16 @@
-# Create volume
+# Create volume web1
 resource "openstack_blockstorage_volume_v3" "CentOS7_vol" {
   region      = "RegionOne"
   name        = "CentOS7_vol"
+  description = "first test volume"
+  size        = 20
+  image_id    = "${openstack_images_image_v2.CentOS7.id}"
+}
+
+# Create volume web2
+resource "openstack_blockstorage_volume_v3" "CentOS7_vol2" {
+  region      = "RegionOne"
+  name        = "CentOS7_vol2"
   description = "first test volume"
   size        = 20
   image_id    = "${openstack_images_image_v2.CentOS7.id}"
--- openstack/0-test/8_floatingip.tf
+++ openstack/0-test/8_floatingip.tf
@@ -6,5 +6,26 @@ resource "openstack_networking_floatingip_v2" "floatip_1" {
 # Connect floating ip and instance
 resource "openstack_compute_floatingip_associate_v2" "fip_1" {
   floating_ip = "${openstack_networking_floatingip_v2.floatip_1.address}"
-  instance_id = "${openstack_compute_instance_v2.web.id}"
+  instance_id = "${openstack_compute_instance_v2.web1.id}"
+}
+
+# Create floating ip 2
+resource "openstack_networking_floatingip_v2" "floatip_2" {
+  pool = "${openstack_networking_network_v2.external_network.name}"
+}
+
+# Connect floating ip and instance 2
+resource "openstack_compute_floatingip_associate_v2" "fip_2" {
+  floating_ip = "${openstack_networking_floatingip_v2.floatip_2.address}"
+  instance_id = "${openstack_compute_instance_v2.web2.id}"
+}
+
+# Create floating ip 3
+resource "openstack_networking_floatingip_v2" "floatip_3" {
+  pool = "${openstack_networking_network_v2.external_network.name}"
+}
+
+resource "openstack_networking_floatingip_associate_v2" "fip_3" {
+  floating_ip = "${openstack_networking_floatingip_v2.floatip_3.address}"
+  port_id     = "${openstack_lb_loadbalancer_v2.centos_lb.vip_port_id}"
 }
